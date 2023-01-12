e9a7b00a466de5916169d875f8888bfd14513798
jonardestamo <117462466+jonardestamo@users.noreply.github.com>
2023-01-13 00:57:30 +0800

    initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,192 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String selectedImagePath = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Image Uploader'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              selectedImagePath == ''
+                  ? Image.asset('asset/image_placeholder.png', height: 200, width: 200, fit: BoxFit.fill,)
+                  : Image.file(File(selectedImagePath), height: 200, width: 200, fit: BoxFit.fill,),
+              const Text(
+                'Select Image',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.all(20)),
+                      textStyle: MaterialStateProperty.all(
+                          const TextStyle(fontSize: 14, color: Colors.white))),
+                  onPressed: () async {
+                    selectImage();
+                    setState(() {});
+                  },
+                  child: const Text('Select')),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      );
+  }
+
+  Future selectImage() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)), //this right here
+            child: Container(
+              height: 150,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Select Image From',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap: () async {
+                            selectedImagePath = await selectImageFromGallery();
+                            print('Image_Path:-');
+                            print(selectedImagePath);
+                            if (selectedImagePath != '') {
+                              Navigator.pop(context);
+                              setState(() {
+                                PermissionStorage();
+                              });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                content: Text("No Image Selected !"),
+                              ));
+                            }
+                          },
+                          child: Card(
+                              elevation: 5,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      'asset/gallery.png',
+                                      height: 60,
+                                      width: 60,
+                                    ),
+                                    const Text('Gallery'),
+                                  ],
+                                ),
+                              )),
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            selectedImagePath = await selectImageFromCamera();
+                            print('Image_Path:-');
+                            print(selectedImagePath);
+
+                            if (selectedImagePath != '') {
+                              Navigator.pop(context);
+                              setState(() {
+                                PermissionCamera();
+                              });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                                content: Text("No Image Captured !"),
+                              ));
+                            }
+                          },
+                          child: Card(
+                              elevation: 5,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    Image.asset(
+                                      'asset/camera.png',
+                                      height: 60,
+                                      width: 60,
+                                    ),
+                                    const Text('Camera'),
+                                  ],
+                                ),
+                              )),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  selectImageFromGallery() async {
+    XFile? file = await ImagePicker()
+        .pickImage(source: ImageSource.gallery, imageQuality: 10);
+    if (file != null) {
+      return file.path;
+    } else {
+      return '';
+    }
+  }
+
+  //
+  selectImageFromCamera() async {
+    XFile? file = await ImagePicker()
+        .pickImage(source: ImageSource.camera, imageQuality: 10);
+    if (file != null) {
+      return file.path;
+    } else {
+      return '';
+    }
+  }
+  void PermissionStorage() async {
+    if (await Permission.storage.request().isGranted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+          content: Text("Storage Permission Granted")));
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text("Storage Permission Denied")));
+    }
+  }
+  void PermissionCamera() async {
+    if (await Permission.camera.request().isGranted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+          content: Text("Camera Permission Granted")));
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text("Camera Permission Denied")));
+    }
+  }
+}
